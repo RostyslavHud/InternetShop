b43c4ad9139a70f52892dcd84fba4453b1ded168
RostyslavHud <rostyslav.hud@gmail.com>
2021-01-18 14:26:17 +0200

    added url variable

--- src/main/java/internetshop/scheduler/UserScheduler.java
+++ src/main/java/internetshop/scheduler/UserScheduler.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserScheduler {
 
-    
+
 }
--- src/main/java/internetshop/service/implementation/EmailServiceImpl.java
+++ src/main/java/internetshop/service/implementation/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package internetshop.service.implementation;
 import internetshop.model.VerificationToken;
 import internetshop.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -22,6 +23,9 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Value("${url}")
+    String url;
+
 
     @Async
     @Override
@@ -29,6 +33,7 @@ public class EmailServiceImpl implements EmailService {
 
         Context context = new Context();
         context.setVariable("token", verificationToken);
+        context.setVariable("tokenUrl", url + "/confirm/" + verificationToken.getToken());
 
         String process = templateEngine.process("email-template/confirm-registration", context);
 
