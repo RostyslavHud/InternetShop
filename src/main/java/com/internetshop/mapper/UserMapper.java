6d0ab46409a0131a16a615a3f4de6c5e7f51a5b8
RostyslavHud <rostyslav.hud@gmail.com>
2021-01-20 10:57:24 +0200

    removed mapping parametr annotation

--- src/main/java/com/internetshop/mapper/UserMapper.java
+++ src/main/java/com/internetshop/mapper/UserMapper.java
@@ -4,26 +4,12 @@ import com.internetshop.dto.RegistrationUserDTO;
 import com.internetshop.dto.SimpleUserDTO;
 import com.internetshop.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mappings({
-            @Mapping(target = "id", source = "user.id"),
-            @Mapping(target = "name", source = "user.name")
-    })
     SimpleUserDTO userToSimpleUser(User user);
 
-    @Mappings({
-            @Mapping(target = "name", source = "user.name"),
-            @Mapping(target = "password", source = "user.password"),
-            @Mapping(target = "firstName", source = "user.firstName"),
-            @Mapping(target = "lastName", source = "user.lastName"),
-            @Mapping(target = "email", source = "user.email"),
-            @Mapping(target = "age", source = "user.age")
-    })
     User registrationUserToUser(RegistrationUserDTO user);
 }
